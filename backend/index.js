e251f4a3b268933668e41f014ba716fe918002e1
gag3116 <lps0524@icloud.com>
2024-10-05 22:24:45 +1300

    Modify mangoose。connect

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ app.use(express.json());
 const port = process.env.PORT || 5001;
 
 // 连接 MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
+
 
 // 引入路由
 const authRoutes = require('./routes/authRoutes');
